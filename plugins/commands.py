52d5a0a7da319b64dac296e9562da6c365a7c473
akshay695 <102814990+akshay695@users.noreply.github.com>
2022-04-09 06:03:05 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -243,12 +243,12 @@ async def help(client, message):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("How to Deploy?", url="https://youtu.be/hkmc3e7U7R4"),
+                    InlineKeyboardButton("Filter bot 2", url="http://t.me/NickyXbot"),
                     InlineKeyboardButton("About Me", callback_data="about_data")
                 ],
                 [
-                    InlineKeyboardButton("BOT Channel", url="https://t.me/TroJanzHEX"),
-                    InlineKeyboardButton("Support Group", url="https://t.me/TroJanzSupport")
+                    InlineKeyboardButton("Movie Group 1", url="https://t.me/+CG7AQS6IfUNhYTNl"),
+                    InlineKeyboardButton("Movie Group 2", url="https://t.me/+Da3C2FnuzP1jZjNl")
                 ]
             ]
         ),
@@ -265,7 +265,7 @@ async def about(client, message):
             [
                 [
                     InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Unlimited-Filter-Bot")
+                        "Update Channel", url="https://t.me/+1qdEeHOTLdQ1M2Vl")
                 ],
                 [
                     InlineKeyboardButton("BACK", callback_data="help_data"),
